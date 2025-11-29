c1a56a4f61ec553b6be3ec615ca1aa68b0f1f9b6
Tuan Dao <tuan.dao.fr@gmail.com>
2025-11-30 00:00:49 +0700

    Add Fairseq to local source

--- run.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-REM Activate virtual environment
-call venv\Scripts\activate
-
-REM Run the inference script
-python app.py
--- run.sh
+++
@@ -1,5 +0,0 @@
-# Activate virtual environment
-source my_env/bin/activate
-
-# Run the inference script
-python app.py
